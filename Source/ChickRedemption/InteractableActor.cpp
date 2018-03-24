679b906bd0248b58eea4e867100100447ed5586d
Chibitybibityba <chrisjokim@gmail.com>
2018-03-24 02:00:05 -0400

    Working Copy of Overlap Event

--- Source/ChickRedemption/InteractableActor.cpp
+++ Source/ChickRedemption/InteractableActor.cpp
@@ -1,14 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "InteractableActor.h"
+#include "Components/ShapeComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "ChickRedemptionCharacter.h"
+#include "Engine.h"
+
 
 
 // Sets default values
 AInteractableActor::AInteractableActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	Root = CreateDefaultSubobject<USceneComponent>("RootComponent");
+	RootComponent = Root;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetupAttachment(Root);
+
+	Collision = CreateDefaultSubobject<UBoxComponent>("BoxCollision");
+	Collision->SetupAttachment(Root);
+	Collision->SetWorldScale3D(FVector(5.0f, 1.0f, 1.0f));
+	Collision->bGenerateOverlapEvents = true;
+
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &AInteractableActor::OnOverlapBegin);
+	Collision->OnComponentEndOverlap.AddDynamic(this, &AInteractableActor::OnOverlapEnd);
+	
 }
 
 // Called when the game starts or when spawned
@@ -23,5 +43,16 @@ void AInteractableActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AInteractableActor::OnOverlapBegin(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("BeginOverlap"));
+	
+}
+
+void AInteractableActor::OnOverlapEnd(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("EndOverlap"));
 }
 
--- Source/ChickRedemption/InteractableActor.h
+++ Source/ChickRedemption/InteractableActor.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "InteractableActor.generated.h"
 
+
 UCLASS()
 class CHICKREDEMPTION_API AInteractableActor : public AActor
 {
@@ -23,6 +24,38 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
+		USceneComponent* Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
+		UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
+		UShapeComponent* Collision;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Pickup)
+		AActor* SpecificActor;
+
+	AChickRedemptionCharacter* LocalRef;
+
+	//Call bakc or event
+		//When actor is spawned, (post-init), then look for the actor
+
+	UFUNCTION()
+		void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
+			AActor* OtherActor,
+			UPrimitiveComponent* OtherComp,
+			int32 OtherBodyIndex,
+			bool bFromSweep,
+			const FHitResult &SweepResult);
 	
-	
+
+	UFUNCTION()
+		void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, 
+			class AActor* OtherActor, 
+			class UPrimitiveComponent* OtherComp, 
+			int32 OtherBodyIndex);
+
+
+
 };
